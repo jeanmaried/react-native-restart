08a475bfdd0f370160229e609e83a5a70f84e044
Avishay Bar <avishay.il@gmail.com>
2020-12-20 18:58:38 +0200

    - Updated to RN 0.63.5

---
+++ Example/App.js
@@ -0,0 +1,106 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+
+import {
+  Header,
+  Colors,
+} from 'react-native/Libraries/NewAppScreen';
+
+import RNRestart from 'react-native-restart';
+
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          {global.HermesInternal == null ? null : (
+            <View style={styles.engine}>
+              <Text style={styles.footer}>Engine: Hermes</Text>
+            </View>
+          )}
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+              <Text style={styles.sectionTitle}>React Native Restart Example App</Text>
+              <Text style={styles.sectionDescription}>
+                  To restart the app, press on this button:
+              </Text>
+              <TouchableOpacity onPress={() => RNRestart.Restart()}>
+                <View style={styles.restartButton}>
+                <Text>Restart</Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  restartButton: {
+    backgroundColor: '#7d7d7d',
+    paddingHorizontal: 50,
+    paddingVertical: 20,
+    marginTop: 10,
+    borderRadius: 10,
+  },
+});
+
+export default App;
--- Example/metro.config.js
+++ Example/metro.config.js
@@ -1,3 +1,10 @@
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
 const path = require('path');
 const fs = require('fs');
 const blacklist = require('metro-config/src/defaults/blacklist');
@@ -30,12 +37,11 @@ module.exports = {
       return acc;
     }, {}),
   },
-
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: true,
+        inlineRequires: false,
       },
     }),
   },
